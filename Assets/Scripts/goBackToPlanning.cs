ac3a65d4e32fe8b2bed88eed7eb0dd73cc8cc001
Lidor Borofker <lidor.borofker@windowslive.com>
2025-02-06 14:21:48 +0200

    fix

---
+++ Assets/Scripts/goBackToPlanning.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class goBackToPlanning : MonoBehaviour
+{
+    public void clicker()
+    {
+        SceneManager.LoadScene("Stratigic Action");
+    }
+}
